1955827bc42f212e751e6db84d32a7f6f0193534
Will Coates <wcoates@ucsc.edu>
2018-06-07 16:20:51 -0700

    changing theoretical_benefit to include three new csv files and excluding transportation, also sample bar chart with weird alignment

--- phaseIII/prototype.js
+++ phaseIII/prototype.js
@@ -118,6 +118,100 @@ var svg = d3.select("body")
             .attr("width", w)
             .attr("height", h);
 
+
+/*
+  Mike Bostock's stacked Bar Chart example: 
+  */
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right - 1000,
+    height = +svg.attr("height") - margin.top - margin.bottom - 200,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .paddingInner(0.05)
+    .align(0.1);
+
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+
+d3.csv("bostockcsv.csv", function(d, i, columns) {
+  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+  d.total = t;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  var keys = data.columns.slice(1);
+
+  data.sort(function(a, b) { return b.total - a.total; });
+  x.domain(data.map(function(d) { return d.State; }));
+  y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
+  z.domain(keys);
+
+  g.append("g")
+    .selectAll("g")
+    .data(d3.stack().keys(keys)(data))
+    .enter().append("g")
+      .attr("fill", function(d) { return z(d.key); })
+    .selectAll("rect")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+      .attr("x", function(d) { return x(d.data.State); })
+      .attr("y", function(d) { return y(d[1]); })
+      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+      .attr("width", x.bandwidth());
+
+  g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(null, "s"))
+    .append("text")
+      .attr("x", 2)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "0.32em")
+      .attr("fill", "#000")
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "start")
+      .text("Population");
+
+  var legend = g.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
+});
+
+
+
+/*
+  Bostock bar chart over
+  */
+
 //Define Tooltip here
 /* initialize the tooltip so that we can move it around
        later */
@@ -126,6 +220,8 @@ var div = d3.select("body").append("div")
 .attr("class", "tooltip")
 .classed("hidden", true);
 */
+
+
 
 
 // color scale for showing population density appropriately:
@@ -213,7 +309,7 @@ d3.json("gz_2010_us_050_00_500k.json", function(json){
 // prototype bar chart
 //Original data
 
-var stackedBar = false;
+var stackedBar = true;
 
 if(stackedBar){
     console.log('using bar chart');
@@ -282,7 +378,7 @@ if(stackedBar){
 		svg.append("text")
             .attr("x", barHorizOffset)
             .attr("y", barVertOffset - 20)
-            .text("Market Share");
+            .text("US Imports Over Time");
     
 		svg.append("text")
             .attr("x", barHorizOffset)
@@ -290,65 +386,4 @@ if(stackedBar){
             .text("Legend");
 }
 
-else{
-    console.log("using pie chart");
-    
-    //var pieDataset = [{name:"US", number: 5}, {name:"China", number: 10}, {name:"World", number: 20}];
-    var pieDataset = [5, 10, 20];
-    
-			var outerRadius = pieRadius;
-			var innerRadius = 0;
-            var pieHorizTranslate = outerRadius + pieHorizOffset;
-            var pieVertTranslate = outerRadius + pieVertOffset;
-    
-			var arc = d3.arc()
-						.innerRadius(innerRadius)
-						.outerRadius(outerRadius);
-			
-			var pie = d3.pie();
-			
-			//Easy colors accessible via a 10-step ordinal scale
-			var pieColor = d3.scaleOrdinal(d3.schemeCategory10);
 
-			//Create SVG element
-			//Set up groups
-			var arcs = svg.selectAll("g.arc")
-						  .data(pie(pieDataset))
-						  .enter()
-						  .append("g")
-						  .attr("class", "arc")
-						  .attr("transform", "translate(" + pieHorizTranslate + "," + pieVertTranslate + ")");
-    
-			
-			//Draw arc paths
-			arcs.append("path")
-			    .attr("fill", function(d, i) {
-			    	return pieColor(i);
-			    })
-			    .attr("d", arc);
-			
-			//Labels
-			arcs.append("text")
-			    .attr("transform", function(d) {
-			    	return "translate(" + arc.centroid(d) + ")";
-			    })
-			    .attr("text-anchor", "middle")
-			    .text(function(d, i) {
-                    console.log(i);
-                    var name;
-                    if(i == 0) name = "US";
-                    else if (i == 1) name = "China";
-                    else name = "World";
-			    	return name + " " + d.value;
-			    });
-    
-		svg.append("text")
-            .attr("x", barHorizOffset)
-            .attr("y", barVertOffset - 20)
-            .text("Market Share");
-    
-		svg.append("text")
-            .attr("x", pieHorizOffset + pieRadius)
-            .attr("y", pieVertOffset + 300)
-            .text("Legend");
-}
